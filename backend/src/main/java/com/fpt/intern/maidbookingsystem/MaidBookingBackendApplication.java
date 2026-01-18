31e3f8c8a8ddadaf7e8ed817fc55361f63217eb3
gwynnn297 <phuchoang29072000@gmail.com>
2026-01-18 21:36:32 +0700

    setup backend spring boot

---
+++ backend/src/main/java/com/fpt/intern/maidbookingsystem/MaidBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.intern.maidbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaidBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaidBookingBackendApplication.class, args);
+	}
+
+}
